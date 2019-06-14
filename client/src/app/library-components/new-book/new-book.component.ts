2d3213d817fa408bfd1407c3a725e940193c46f2
ADJACENT\QDean <qdean@adjacent-tech.com>
2019-06-14 11:28:35 -0500

    add bulk excel processing

--- client/src/app/library-components/new-book/new-book.component.ts
+++ client/src/app/library-components/new-book/new-book.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { FormBuilder, FormGroup, FormArray } from "@angular/forms";
 import { Router } from "@angular/router";
 import { Subject } from "rxjs";
@@ -7,14 +7,17 @@ import { BookService } from "../book.service";
 
 import { BookModel } from "../book.model";
 
+import * as XLSX from "xlsx";
+
 @Component({
   selector: "app-new-book",
   templateUrl: "./new-book.component.html",
   styleUrls: ["./new-book.component.scss"]
 })
-export class NewBookComponent implements OnInit {
+export class NewBookComponent implements OnInit, OnDestroy {
   destroy$: Subject<void> = new Subject<void>();
 
+  bookJson: [];
   bookForm: FormGroup;
   constructor(
     private router: Router,
@@ -65,8 +68,38 @@ export class NewBookComponent implements OnInit {
       });
   }
 
-  goBack() {
-    this.router.navigateByUrl("/");
+  handleExcelFile(event) {
+    const files = event.target.files;
+    const f = files[0];
+    const reader = new FileReader();
+    let json = null;
+    reader.onload = e => {
+      const data = new Uint8Array(e.target["result"]);
+      const workbook = XLSX.read(data, { type: "array" });
+      json = XLSX.utils.sheet_to_json(workbook.Sheets["Sheet1"]);
+      this.bookJson = json;
+    };
+    reader.readAsArrayBuffer(f);
+  }
+
+  submitBulkUpload() {
+    if (this.bookJson !== null) {
+      const bookModels = this.bookJson.map((book: any) => {
+        const splitAuthors = book.authors ? book.authors.split(",") : null;
+        const newBook: BookModel = new BookModel(
+          book.lccn,
+          book.isbn,
+          book.title,
+          splitAuthors,
+          book.publishDate,
+          true
+        );
+        return newBook;
+      });
+      console.log(bookModels);
+    } else {
+      // error
+    }
   }
 
   ngOnDestroy() {
